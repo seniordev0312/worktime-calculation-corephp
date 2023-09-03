d907eac9abf49df5e421aab7db9d413a31608ce6
seniordev0312 <devsenior0312@gmail.com>
2023-09-03 17:57:32 +0100

    update

--- get_staff_data.php
+++ get_staff_data.php
@@ -12,7 +12,7 @@
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      if($row['LEFT'] == '0000-00-00') {
+      if($row['LEFT'] == '0000-00-00' || !$row['LEFT']) {
         
         echo json_encode($row);
       }
