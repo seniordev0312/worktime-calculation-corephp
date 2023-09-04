d88823c82a859ab3bac518f05e393e739540aee7
seniordev0312 <devsenior0312@gmail.com>
2023-09-04 19:33:00 +0100

    update the mail function

--- request_time.php
+++ request_time.php
@@ -1,10 +1,11 @@
 <?php
+ini_set("SMTP", "smtp.ionos.de");
+ini_set("sendmail_from", "devsonspree@gmail.com");
 
-mail('devsonspree@gmail.com', 'My Subject', 'Hi');
-if (!$success) {
-  $errorMessage = error_get_last()['message'];
-} else {
-  echo "1";
-}
+$message = "The mail message was sent with the following mail setting:\r\nSMTP = aspmx.l.google.com\r\nsmtp_port = 25\r\nsendmail_from = YourMail@address.com";
 
+$headers = "From: devsonspree@gmail.com";
+
+mail("devsenior0312@gmail.com", "Testing", $message, $headers);
+echo "Check your email now....&lt;BR/>";
 ?>
