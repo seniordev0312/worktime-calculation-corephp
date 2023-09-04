c77b2225df9a1b7ce828af2a1db5d86a433485f1
seniordev0312 <devsenior0312@gmail.com>
2023-09-04 20:05:05 +0100

    update mail function

--- request_time.php
+++ request_time.php
@@ -1,11 +1,33 @@
 <?php
-ini_set("SMTP", "smtp.ionos.de");
-ini_set("sendmail_from", "devsonspree@gmail.com");
-
-$message = "The mail message was sent with the following mail setting:\r\nSMTP = aspmx.l.google.com\r\nsmtp_port = 25\r\nsendmail_from = YourMail@address.com";
-
-$headers = "From: devsonspree@gmail.com";
-
-mail("devsenior0312@gmail.com", "Testing", $message, $headers);
-echo "Check your email now....&lt;BR/>";
-?>
+require_once "vendor/autoload.php";
+$mail = new PHPMailer;
+//Enable SMTP debugging.
+$mail->SMTPDebug = 3;                           
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();        
+//Set SMTP host name                      
+$mail->Host = "smtp.ionos.de";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                      
+//Provide username and password
+$mail->Username = "ueberstunden@cutman-friseur.de";             
+$mail->Password = "123.456.Aliveli07";                       
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                       
+//Set TCP port to connect to
+$mail->Port = 465;                    
+$mail->From = "devsonspree@gmail.com";
+$mail->FromName = "Full Name";
+$mail->addAddress("devsenior@gmail.com", "Recepient Name");
+$mail->isHTML(true);
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+if(!$mail->send())
+{
+echo "Mailer Error: " . $mail->ErrorInfo;
+}
+else
+{
+echo "Message has been sent successfully";
+}
