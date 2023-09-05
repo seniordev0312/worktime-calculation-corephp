fd0a17ec7aeababaa4f66394c3f9f190cd9440d0
seniordev0312 <devsenior0312@gmail.com>
2023-09-05 04:42:54 +0100

    update the env type

--- request_time.php
+++ request_time.php
@@ -10,19 +10,19 @@ $mail = new PHPMailer(true);
 //Set PHPMailer to use SMTP.
 $mail->isSMTP();        
 //Set SMTP host name                      
-$mail->Host = $env['SMTP_HOST'];
+$mail->Host = strval($env['SMTP_HOST']);
 //Set this to true if SMTP host requires authentication to send email
 $mail->SMTPAuth = true;                      
 //Provide username and password
-$mail->Username = $env['SMTP_USER'];             
-$mail->Password = $env['SMTP_PASS'];                       
+$mail->Username = strval($env['SMTP_USER']);             
+$mail->Password = strval($env['SMTP_PASS']);                       
 //If SMTP requires TLS encryption then set it
 $mail->SMTPSecure = "tls";                       
 //Set TCP port to connect to
 $mail->Port = int($env['SMTP_PORT']);                    
-$mail->From = $env['SENDER_MAIL'];
+$mail->From = strval($env['SENDER_MAIL']);
 $mail->FromName = $_GET['name'] . " ". $_GET['surname'];
-$mail->addAddress($env['RECEIVER_MAIL'], "Ugur");
+$mail->addAddress(strval($env['RECEIVER_MAIL']), "Ugur");
 $mail->isHTML(true);
 $mail->Subject = "Time Request";
 $mail->Body = "<h2>Hi, Ugur!</h2>";
