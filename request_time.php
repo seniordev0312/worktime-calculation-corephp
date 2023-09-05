1a988d86ff0596871053b55543fd8fcdc2f08650
seniordev0312 <devsenior0312@gmail.com>
2023-09-05 08:56:31 +0100

    update the table

--- request_time.php
+++ request_time.php
@@ -5,6 +5,10 @@ require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+include('db.php');
+
+$date_day = date("Y-m-d");
+
 $mail = new PHPMailer(true);
                   
 //Set PHPMailer to use SMTP.
@@ -29,7 +33,28 @@ $mail->Body = "<h2>Hi, Ugur!</h2>";
 $mail->AltBody = "I want to add " .$_GET['hours']. " more today. Please allow me.";
 
 try {
+  $conn = new mysqli($servername,$username, $password, $databasename);
+
+  // For checking if connection is
+  // successful or not
+  if ($conn->connect_error) {
+    echo "connection failed";
+    die("Connection failed: "
+        . $conn->connect_error);
+  }
+  $hour = (int)$_GET["hours"];
+  $updateQuery = "UPDATE cm_ho_working_plans SET HOURS_TOTAL='$hour' WHERE STAFF_ID ='{$_GET['id']}' AND WORK_DATE = '$date_day'";
+
+  $updateResult = $conn->query($updateQuery);
+} catch (Exception $e) {
+  echo "Time track still not started";
+  exit;
+}
+
+try {
+  
   $mail->send();
+  
   echo 'Request submitted successfully.';
 } catch (Exception $e) {
   echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
