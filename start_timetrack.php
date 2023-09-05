b0594b8970eaf37b0f6c78a88e5bfb5c32a2ba5a
seniordev0312 <devsenior0312@gmail.com>
2023-09-05 15:18:39 +0100

    test

--- start_timetrack.php
+++ start_timetrack.php
@@ -18,8 +18,8 @@ if($_POST['id']) {
   $sql_check = "SELECT * FROM cm_ho_working_plans WHERE STAFF_ID = '{$_POST["id"]}' AND WORK_DATE = '$date_day'";
   $result_check = $conn->query($sql_check);
   if($result_check->num_rows > 0) {
-    $updateQuery = "UPDATE cm_ho_working_plans SET TIME_START='{$date_day}', STAFF_ID='{$_POST["id"]}', TIME_END='00:00:00' WHERE STAFF_ID ='{$_POST['id']}'";
-    $result = $conn->query($updateQuery);
+    // $updateQuery = "UPDATE cm_ho_working_plans SET TIME_START='{$date_day}', STAFF_ID='{$_POST["id"]}', TIME_END='00:00:00' WHERE STAFF_ID ='{$_POST['id']}'";
+    // $result = $conn->query($updateQuery);
     echo "1";
   } else {
     $sql = "INSERT INTO cm_ho_working_plans (STAFF_ID, WORK_DATE, TIME_START) VALUES ('{$_POST["id"]}', '{$date_day}', '{$date_time}')";
