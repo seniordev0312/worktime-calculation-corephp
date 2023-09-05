018e3a8110065e99a380101b2e5c4a8888089a7a
seniordev0312 <devsenior0312@gmail.com>
2023-09-05 02:39:04 +0100

    update the message body and make env file

--- request_time.php
+++ request_time.php
@@ -9,26 +9,26 @@ $mail = new PHPMailer(true);
 //Set PHPMailer to use SMTP.
 $mail->isSMTP();        
 //Set SMTP host name                      
-$mail->Host = "smtp.ionos.de";
+$mail->Host = $_ENV['HOST'];
 //Set this to true if SMTP host requires authentication to send email
 $mail->SMTPAuth = true;                      
 //Provide username and password
-$mail->Username = "ueberstunden@cutman-friseur.de";             
-$mail->Password = "123.456.Aliveli07";                       
+$mail->Username = $_ENV['SMTP_USER'];             
+$mail->Password = $_ENV['SMTP_PASS'];                       
 //If SMTP requires TLS encryption then set it
 $mail->SMTPSecure = "tls";                       
 //Set TCP port to connect to
-$mail->Port = 465;                    
-$mail->From = "devsonspree@gmail.com";
-$mail->FromName = "Full Name";
-$mail->addAddress("devsenior0312@gmail.com", "Recepient Name");
+$mail->Port = $_ENV['SMTP_PORT'];                    
+$mail->From = $_ENV['SENDER_MAIL'];
+$mail->FromName = $_GET['name'] . " ". $_GET['surname'];
+$mail->addAddress($_ENV['RECEIVER_MAIL'], "Ugur");
 $mail->isHTML(true);
-$mail->Subject = "Subject Text";
-$mail->Body = "<i>Mail body in HTML</i>";
-$mail->AltBody = "This is the plain text version of the email content";
+$mail->Subject = "Time Request";
+$mail->Body = "<h2>Hi, Ugur!</h2>";
+$mail->AltBody = "I want to add " .$_GET['hours']. " more today. Please allow me.";
 try {
   $mail->send();
-  echo 'Email sent successfully.';
+  echo 'Request submitted successfully.';
 } catch (Exception $e) {
   echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
 }
