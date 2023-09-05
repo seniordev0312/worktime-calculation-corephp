d11c4134c8279568bac14af98870e8fc99241de8
seniordev0312 <devsenior0312@gmail.com>
2023-09-05 02:02:17 +0100

    update the request backend

--- request_time.php
+++ request_time.php
@@ -1,21 +1,11 @@
 <?php
+require 'vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-/* Exception class. */
-require 'vendor\phpmailer\phpmailer\src\Exception.php';
-
-/* The main PHPMailer class. */
-require 'vendor\phpmailer\phpmailer\src\PHPMailer.php';
-
-/* SMTP class, needed if you want to use SMTP. */
-require 'vendor\phpmailer\phpmailer\src\SMTP.php';
-
-require 'vendor/autoload.php';
-
 $mail = new PHPMailer(true);
-//Enable SMTP debugging.
-$mail->SMTPDebug = 3;                           
+                  
 //Set PHPMailer to use SMTP.
 $mail->isSMTP();        
 //Set SMTP host name                      
@@ -28,7 +18,7 @@ $mail->Password = "Devsenior0312!!!";
 //If SMTP requires TLS encryption then set it
 $mail->SMTPSecure = "tls";                       
 //Set TCP port to connect to
-$mail->Port = 465;                    
+$mail->Port = 587;                    
 $mail->From = "devsonspree@gmail.com";
 $mail->FromName = "Full Name";
 $mail->addAddress("devsenior0312@gmail.com", "Recepient Name");
@@ -36,13 +26,11 @@ $mail->isHTML(true);
 $mail->Subject = "Subject Text";
 $mail->Body = "<i>Mail body in HTML</i>";
 $mail->AltBody = "This is the plain text version of the email content";
-if(!$mail->send())
-{
-echo "Mailer Error: " . $mail->ErrorInfo;
-}
-else
-{
-echo "Message has been sent successfully";
+try {
+  $mail->send();
+  echo 'Email sent successfully.';
+} catch (Exception $e) {
+  echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
 }
 
 ?>
