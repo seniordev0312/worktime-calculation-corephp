f7737eddb0bce15e7feb8dda8c244c2889179651
seniordev0312 <devsenior0312@gmail.com>
2023-09-06 17:05:53 +0100

    udpate

--- request_time.php
+++ request_time.php
@@ -46,23 +46,23 @@ try {
   $sql_check = "SELECT * FROM cm_ho_working_plans WHERE STAFF_ID = '{$_GET['id']}' AND WORK_DATE = '{$_GET["date"]}'";
   $result_check = $conn->query($sql_check);
   if($result_check) {
-    $updateQuery = "UPDATE cm_ho_working_plans SET HOURS_TOTAL='$hour' WHERE STAFF_ID ='{$_GET['id']}' AND WORK_DATE = '$date_day'";
+    // $updateQuery = "UPDATE cm_ho_working_plans SET HOURS_TOTAL='$hour' WHERE STAFF_ID ='{$_GET['id']}' AND WORK_DATE = '$date_day'";
 
-    $updateResult = $conn->query($updateQuery);
-    // echo "0";
+    // $updateResult = $conn->query($updateQuery);
+    echo "0";
   } else {
-    $createQuery = "INSERT INTO cm_ho_working_plans (HOURS_TOTAL, WORK_DATE, STAFF_ID) VALUES ('{$hour}', '{$_GET["date"]}'), '{$_GET['id']}'";
-    $conn->query($createQuery);
-    // echo "1";
+    // $createQuery = "INSERT INTO cm_ho_working_plans (HOURS_TOTAL, WORK_DATE, STAFF_ID) VALUES ('{$hour}', '{$_GET["date"]}'), '{$_GET['id']}'";
+    // $conn->query($createQuery);
+    echo "1";
   }
-  try {
+  // try {
   
-    $mail->send();
+  //   $mail->send();
     
-    echo 'Request submitted successfully.';
-  } catch (Exception $e) {
-    echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
-  }
+  //   echo 'Request submitted successfully.';
+  // } catch (Exception $e) {
+  //   echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
+  // }
   
 } catch (Exception $e) {
   echo "Time track still not started";
