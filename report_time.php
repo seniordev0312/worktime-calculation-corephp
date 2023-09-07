786a0f93dbbdbb2213e8e4964ad41d8b5243e207
seniordev0312 <devsenior0312@gmail.com>
2023-09-07 03:41:14 +0100

    update report button feature

--- report_time.php
+++ report_time.php
@@ -14,21 +14,16 @@ if ($conn->connect_error) {
 $date_time = date("h:i:s");
 $date_day = date("Y-m-d");
 
-$sql = "SELECT TIME_START, WORK_DATE, HOURS_WORK, HOURS_TOTAL from cm_ho_working_plans WHERE STAFF_ID = '{$_GET["id"]}' AND WORK_DATE='$date_day'";
+$sql = "SELECT * from cm_ho_working_plans WHERE STAFF_ID = '{$_GET["id"]}'";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    
-      $hrs = round(abs(strtotime($date_time) - strtotime($row['TIME_START']) + $row['HOURS_WORK'] * 3600) / 3600);
-      $minutes = round(abs(strtotime($date_time) - strtotime($row['TIME_START']) + $row['HOURS_WORK'] * 3600) / 60) - 60*$hrs;
-      if($hrs >= 9) {
-        echo ($hrs - 9) . " hours overworked. Please submit the request!";
-      } else {
-        echo "No overtime yet!";
-      }
+    if($row['HOURS_TOTAL'] != "0.00" || !$row['HOURS_TOTAL']){
+      echo json_encode($row);
+    }
     
   }
 } else {
