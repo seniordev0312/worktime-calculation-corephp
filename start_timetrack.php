6cb29f0b988400b1e24183dd46bed87171b508de
seniordev0312 <devsenior0312@gmail.com>
2023-09-14 04:03:53 +0100

    fix

--- start_timetrack.php
+++ start_timetrack.php
@@ -17,26 +17,27 @@ if($_POST['id']) {
   $date_day = date("Y-m-d");
   $sql_check = "SELECT * FROM cm_ho_working_plans WHERE STAFF_ID = '{$_POST["id"]}' AND WORK_DATE = '{$date_day}'";
   $result_check = $conn->query($sql_check);
-  if($result_check) {
-    $id = (int) $_POST["id"];
-    $updateQuery = "UPDATE cm_ho_working_plans SET TIME_START = '{$date_time}', TIME_END = '{$end_time}' WHERE STAFF_ID = '{$id}' AND WORK_DATE = '{$date_day}'";
-    $result = $conn->query($updateQuery);
-    if (!$result) {
-      echo $result;
-        die('Error: ' . $conn->error);
-    }
-    echo "1";
+  echo $result_check->num_rows;
+  // if($result_check->num_rows > 0) {
+  //   $id = (int) $_POST["id"];
+  //   $updateQuery = "UPDATE cm_ho_working_plans SET TIME_START = '{$date_time}', TIME_END = '{$end_time}' WHERE STAFF_ID = '{$id}' AND WORK_DATE = '{$date_day}'";
+  //   $result = $conn->query($updateQuery);
+  //   if (!$result) {
+  //     echo $result;
+  //       die('Error: ' . $conn->error);
+  //   }
+  //   echo "1";
     
 
-  } else {
-    $sql = "INSERT INTO cm_ho_working_plans (STAFF_ID, WORK_DATE, TIME_START) VALUES ('{$_POST["id"]}', '{$date_day}', '{$date_time}')";
-    $result = $conn->query($sql);
-    // if (!$result) {
-    //   echo $result;
-    //     die('Error: ' . $conn->error);
-    // }
-    echo "0";
-  } 
+  // } else {
+  //   $sql = "INSERT INTO cm_ho_working_plans (STAFF_ID, WORK_DATE, TIME_START) VALUES ('{$_POST["id"]}', '{$date_day}', '{$date_time}')";
+  //   $result = $conn->query($sql);
+  //   // if (!$result) {
+  //   //   echo $result;
+  //   //     die('Error: ' . $conn->error);
+  //   // }
+  //   echo "0";
+  // } 
 
   } 
   // echo "1111";
