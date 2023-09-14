56262a0f805310c683c85812a3be486bd74048c6
seniordev0312 <devsenior0312@gmail.com>
2023-09-14 03:32:32 +0100

    test

--- start_timetrack.php
+++ start_timetrack.php
@@ -30,11 +30,11 @@ if($_POST['id']) {
 
   } else {
     $sql = "INSERT INTO cm_ho_working_plans (STAFF_ID, WORK_DATE, TIME_START) VALUES ('{$_POST["id"]}', '{$date_day}', '{$date_time}')";
-    $result = $conn->query($sql);
-    if (!$result) {
-      echo $result;
-        die('Error: ' . $conn->error);
-    }
+    // $result = $conn->query($sql);
+    // if (!$result) {
+    //   echo $result;
+    //     die('Error: ' . $conn->error);
+    // }
     echo "0";
   } 
 
